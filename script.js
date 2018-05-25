42565a551af0871370078f5c9088c67f28bd3bc6
David Ornellas <7051351+Ornellasd@users.noreply.github.com>
2018-05-25 15:03:09 -0700

    adds weather condition

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var lat = "lat=" + Math.round(position.coords.latitude);
-      var lon = "lon=" + Math.round(position.coords.longitude);
+      var lat = "lat=" + Math.round(position.coords.latitude * 100) / 100;
+      var lon = "lon=" + Math.round(position.coords.longitude* 100) / 100;
       var keyword = "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon;
 
       $.getJSON(keyword, function(json) {
@@ -10,10 +10,14 @@ $(document).ready(function() {
         var fahrenheit = temp * 9 / 5 + 32 + "F";
         var humidity = json.main.humidity;
         var icon = json.weather[0].icon;
+        var location = json.name;
+        var condition = json.weather[0].description;
 
         $("#temperature").text("Temperature:" + " " + fahrenheit);
         $("#humidity").text("Humidty:" + " " + humidity + "%");
         $("#icon-image").append("<img src='" + icon + "' />");
+        $("#location").text(location);
+        $("#condition").text(condition);
 
         $("#celsius").click(function() {
           $("#temperature").text("Temperature:" + " " + temp + "C");
