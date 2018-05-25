5862b72770423b122af1cdb56d7c732dccd87a51
David Ornellas <7051351+Ornellasd@users.noreply.github.com>
2018-05-25 14:45:41 -0700

    adds toggles to change temperature to celsius or fahrenheit

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = "lat=" + Math.round(position.coords.latitude);
+      var lon = "lon=" + Math.round(position.coords.longitude);
+      var keyword = "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon;
 
-   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-    var lat = "lat=" + Math.round(position.coords.latitude);
-    var lon = "lon=" + Math.round(position.coords.longitude);
-    var keyword = "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon;
-
-    $.getJSON(keyword, function(json) {
-        var temp = json.main.temp;
-        var fahrenheit = temp * 9/5 + 32 + "F";
+      $.getJSON(keyword, function(json) {
+        var temp = Math.round(json.main.temp);
+        var fahrenheit = temp * 9 / 5 + 32 + "F";
         var humidity = json.main.humidity;
         var icon = json.weather[0].icon;
+
         $("#temperature").text("Temperature:" + " " + fahrenheit);
         $("#humidity").text("Humidty:" + " " + humidity + "%");
         $("#icon-image").append("<img src='" + icon + "' />");
+
+        $("#celsius").click(function() {
+          $("#temperature").text("Temperature:" + " " + temp + "C");
+        });
+        $("#fahrenheit").click(function() {
+          $("#temperature").text("Temperature:" + " " + fahrenheit);
+        })
       });
+
     });
-   } else {
+
+  } else {
     console.log("Geolocation is not supported by this browser.");
   }
 });
-
