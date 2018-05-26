1a9dc4f9786ac71162242ba51b4f710c0b7208bf
David Ornellas <7051351+Ornellasd@users.noreply.github.com>
2018-05-26 11:52:00 -0700

    adds ajax

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
+function ajax(keyword) {
+  $.ajax({
+    type: "GET",
+    url: keyword,
+    dataType: "jsonp",
+    success: function(data) {
+      console.log(data);
+      showWeather(data);
+    },
+    error: function() {
+      console.log("ERROR");
+    }
+  });
+}
+
+function showWeather(callback) {
+  var temp = Math.round(callback.main.temp);
+  var fahrenheit = temp * 9 / 5 + 32 + "F";
+  var humidity = callback.main.humidity;
+  var icon = callback.weather[0].icon;
+  var location = callback.name;
+  var condition = callback.weather[0].description.split(" ");
+
+  $("#temperature").text("Temperature:" + " " + fahrenheit);
+  $("#humidity").text("Humidty:" + " " + humidity + "%");
+  $("#icon-image").append("<img src='" + icon + "' />");
+  $("#location").text(location);
+  for (var i = 0; i < condition.length; i++) {
+    var conditionFormat = condition[i][0].toUpperCase() + condition[i].slice(1, condition[i].length);
+    $("#condition").text(conditionFormat);
+  }
+
+  $("#celsius").click(function() {
+    $("#temperature").text("Temperature:" + " " + temp + "C");
+  });
+  $("#fahrenheit").click(function() {
+    $("#temperature").text("Temperature:" + " " + fahrenheit);
+  })
+}
+
 $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var lat = "lat=" + Math.round(position.coords.latitude * 100) / 100;
-      var lon = "lon=" + Math.round(position.coords.longitude* 100) / 100;
+      var lon = "lon=" + Math.round(position.coords.longitude * 100) / 100;
       var keyword = "https://fcc-weather-api.glitch.me/api/current?" + lat + "&" + lon;
-
-      $.getJSON(keyword, function(json) {
-        var temp = Math.round(json.main.temp);
-        var fahrenheit = temp * 9 / 5 + 32 + "F";
-        var humidity = json.main.humidity;
-        var icon = json.weather[0].icon;
-        var location = json.name;
-        var condition = json.weather[0].description;
-
-        $("#temperature").text("Temperature:" + " " + fahrenheit);
-        $("#humidity").text("Humidty:" + " " + humidity + "%");
-        $("#icon-image").append("<img src='" + icon + "' />");
-        $("#location").text(location);
-        $("#condition").text(condition);
-
-        $("#celsius").click(function() {
-          $("#temperature").text("Temperature:" + " " + temp + "C");
-        });
-        $("#fahrenheit").click(function() {
-          $("#temperature").text("Temperature:" + " " + fahrenheit);
-        })
-      });
-
+      ajax(keyword);
     });
-
   } else {
-    console.log("Geolocation is not supported by this browser.");
+    alert("Geolocation is not supported by this browser.");
   }
 });
